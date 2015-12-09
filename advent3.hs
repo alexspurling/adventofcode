ba8b64027e8ad52e4f371e885674bb9e773d579f
Alex Spurling <alexspurling@gmail.com>
2015-12-09 21:31:57 +0000

    Part 2 of day 3

--- advent3.hs
+++ advent3.hs
@@ -1,9 +1,11 @@
-import Data.Map as M
+import qualified Data.Map as M
 
 main = do
   input <- readFile "advent3.txt"
   let numberOfHouses = getNumberOfHouses input
+  let roboNumberOfHouses = getRoboNumberOfHouses input
   putStrLn ("Santa visited " ++ (show numberOfHouses) ++ " lucky people this year")
+  putStrLn ("Santa and robo santa visited " ++ (show roboNumberOfHouses) ++ " lucky people this year")
 
 getNumberOfHouses :: String -> Int
 getNumberOfHouses input =
@@ -11,6 +13,18 @@ getNumberOfHouses input =
     visitMap = visitHouses (0, 0) M.empty input
   in
     countVisits visitMap
+
+getRoboNumberOfHouses :: String -> Int
+getRoboNumberOfHouses input =
+  let
+    --Santa follows every odd direction
+    santaDirections = each 2 input
+    --Robo santa follows every even direction
+    roboDirections = each 2 $ tail input
+    santaVisitMap = visitHouses (0, 0) M.empty santaDirections
+    roboSantaVisitMap = visitHouses (0, 0) santaVisitMap roboDirections
+  in
+    countVisits roboSantaVisitMap
 
 type VisitMatrix = M.Map Int (M.Map Int Int)
 
@@ -40,3 +54,6 @@ visitHouses (x, y) visitMap (nextDirection:remainingDirections) =
 countVisits :: VisitMatrix -> Int
 countVisits visitMap =
   M.foldr (\m visitCount -> (M.size m + visitCount)) 0 visitMap
+
+each :: Int -> [a] -> [a]
+each n = map head . takeWhile (not . null) . iterate (drop n)
