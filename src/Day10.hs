84d19586746ed0c98c74f8b3219684a1451c973b
Alex Spurling <alexspurling@gmail.com>
2016-01-17 18:00:02 +0000

    Slightly faster solution for Day10 (~2.6s vs 2.9s)
    Instead of:
      show (length g) ++ head g
    
    I am doing
      head g : show (length g)
    
    This however makes very little difference because the size of the groups is never more than 3.
    Also it computes the reverse of the expected output but that's ok as we're only interested
    in finding the final size.

--- src/Day10.hs
+++ src/Day10.hs
@@ -7,10 +7,10 @@ lookAndSay string =
   --Group chars in string
   --Then count the number of elements in each group
   --Add the count followed by the element to new list
-  concat $ map (\g -> show (length g) ++ [(head g)]) (group string)
+  concat $ map (\g -> (head g) : show (length g)) (group string)
 
 nthLookAndSay :: String -> Int -> String
 nthLookAndSay input n = (iterate lookAndSay input) !! n
 
 lookAndSayLength :: String -> Int -> Int
-lookAndSayLength input n = length $ nthLookAndSay input n
+lookAndSayLength input n = length $ nthLookAndSay (reverse input) n
