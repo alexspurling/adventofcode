4b3cc2073c7d669f9e277c986d27ae37056c5ffe
Alex Spurling <alexspurling@gmail.com>
2016-01-20 21:35:38 +0000

    Day 13 part 2

--- src/Day13.hs
+++ src/Day13.hs
@@ -13,29 +13,45 @@ happinessMatrix =
    [ -4, 23,-35, 75, 34, 18,  0,-99],
    [-80, 29, 10,-20,-99,-56,  7,  0]]
 
+happinessMatrix2 :: [[Int]]
+happinessMatrix2 =
+  [[  0, 93,-54, 43,  8,-97, 45, 31, 0],
+   [ -2,  0,-70,-96,-89,  6, 76,-32, 0],
+   [-62, 19,  0,-53,-69, -9, 63, 95, 0],
+   [ 65,  5,-37,  0,-34, 56, 54, 91, 0],
+   [ 21, 49,-46,-30,  0,-17, 54,-66, 0],
+   [-81, 68, 33,-12, 95,  0, 30,-75, 0],
+   [ -4, 23,-35, 75, 34, 18,  0,-99, 0],
+   [-80, 29, 10,-20,-99,-56,  7,  0, 0],
+   [  0,  0,  0,  0,  0,  0,  0,  0, 0]]
+
 -- We use cycle here because we want to pair up the
 -- final guest with the first.
 paring guests = zip guests (tail (cycle guests))
 
-happiness pairs =
+happiness matrix pairs =
   map guestHappiness pairs
   where
     guestHappiness (guest1, guest2) =
       let
-        happiness1 = (happinessMatrix!!guest1)!!guest2
-        happiness2 = (happinessMatrix!!guest2)!!guest1
+        happiness1 = (matrix!!guest1)!!guest2
+        happiness2 = (matrix!!guest2)!!guest1
       in
         happiness1 + happiness2
 
-totalHappiness =
+totalHappiness matrix =
   let
-    numGuests = length happinessMatrix - 1
+    numGuests = length matrix - 1
     guestOrderings = permutations [0..numGuests]
     allParings = map paring guestOrderings
-    allHappiness = map happiness allParings
+    allHappiness = map (happiness matrix) allParings
   in
     map sum allHappiness
 
 maximumGuestHappiness :: Int
 maximumGuestHappiness =
-  maximum totalHappiness
+  maximum $ totalHappiness happinessMatrix
+
+maximumGuestHappinessIncludingMe :: Int
+maximumGuestHappinessIncludingMe =
+  maximum $ totalHappiness happinessMatrix2
--- test/Day13Spec.hs
+++ test/Day13Spec.hs
@@ -8,6 +8,8 @@ spec =
   describe "Day13" $ do
     it "Maximum guest happiness is 664" $ do
       Day13.maximumGuestHappiness `shouldBe` 664
+    it "Maximum guest happiness including me is 640" $ do
+      Day13.maximumGuestHappinessIncludingMe `shouldBe` 640
 
 specIO :: IO Spec
 specIO = return spec
